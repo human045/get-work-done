ab4f9390c81ab906036061285aa799f79fab1e66
human045 <akhileshdixit045@proton.me>
2026-03-27 11:24:05 +0530

    debug: enable PostHog debug mode temporarily

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 posthog.init('phc_SS5g62UUVqA4fgyCprePiSmV0HvsQ7pl6eyFgQhPmMg', {
   api_host: 'https://eu.i.posthog.com',
   defaults: '2026-01-30',
+  debug: true,
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
